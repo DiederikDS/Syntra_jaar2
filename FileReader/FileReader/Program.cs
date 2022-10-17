1d54e0a1112e508a761f61710c6d79527c0deb34
DiederikDS <diederik.desoete96@gmail.com>
2022-10-17 17:46:45 +0200

    sdf
    filereader oef

---
+++ FileReader/FileReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using FileReader.Classes;
+
+DirectoryExplorer de = new DirectoryExplorer();
+de.GetItems(@"C:\Users\Diederik\Desktop");
+
+foreach(var i in de.Items)
+{
+    Console.WriteLine(i);
+}
+
+//--------------------------------------------------------------------
+
+TxtFileReader fr = new TxtFileReader();
+
+fr.ReadFile(@"C:\Users\Diederik\Desktop\Testdoc.txt");
+
+foreach (var item in fr.Lines)
+{
+    Console.WriteLine(item);
+}
