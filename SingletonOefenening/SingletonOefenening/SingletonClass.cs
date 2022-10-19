224a3686b1f1e98f89dc4a85afc2673f7d7af700
DiederikDS <diederik.desoete96@gmail.com>
2022-10-19 19:16:37 +0200

    singleton

---
+++ SingletonOefenening/SingletonOefenening/SingletonClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonOefenening
+{
+    internal class SingletonClass
+    {
+        private static string AangemaaktOp;
+        private static SingletonClass Instance = null;
+
+        private SingletonClass()
+        {
+            AangemaaktOp = DateTime.Now.ToString();
+        }
+
+        public static SingletonClass GetInstance {
+            get
+            {
+                if (Instance == null)
+                {
+                    Instance = new SingletonClass();
+                }
+
+                return Instance;
+            } 
+        }
+        
+        public string Tijd()
+        {
+            return AangemaaktOp.ToString();
+        }
+    }
+}
