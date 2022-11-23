30013f2751ef29db4bafb5723bfb7d1533fb08d1
DiederikDS <diederik.desoete96@gmail.com>
2022-11-23 19:24:47 +0100

    chqnges

---
+++ BetalingenOefening/Betalingen/BetalingenTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BetalingenTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
