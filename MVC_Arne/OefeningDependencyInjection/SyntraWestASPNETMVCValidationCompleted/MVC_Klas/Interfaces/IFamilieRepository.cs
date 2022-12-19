71a785613aba5b7b3bd0b32b6b0f999bcfede3c5
DiederikDS <diederik.desoete96@gmail.com>
2022-12-19 21:23:57 +0100

    change

---
+++ MVC_Arne/OefeningDependencyInjection/SyntraWestASPNETMVCValidationCompleted/MVC_Klas/Interfaces/IFamilieRepository.cs
@@ -0,0 +1,14 @@
+﻿using MVC_Klas.Models;
+
+namespace MVC_Klas.Interfaces
+{
+    public interface IFamilieRepository
+    {
+        public List<Familie> GetAll();
+        public Familie GetByNaam(string naam);
+        public bool AddFamilie(Familie familie);
+        public void UpdateFamilie(Familie familie);
+        public void DeleteFamilie(string naam);
+
+    }
+}
--- MVC_Arne/SyntraWestASPNETMVCIntro/SyntraWestASPNETMVCIntro/MVC_Klas/Models/Familie.cs
+++ MVC_Arne/SyntraWestASPNETMVCIntro/SyntraWestASPNETMVCIntro/MVC_Klas/Models/Familie.cs
@@ -4,11 +4,11 @@ namespace MVC_Klas.Models
 {
     public class Familie
     {
-        [Key]
         public int FamilieId { get; set; }
         [Required]
+        [MaxLength(50)]
         public string Naam { get; set; }
-        [Required]
+        [StringLength(30,MinimumLength = 10)]
         public string Orde { get; set; }
         [Required]
         public string Klasse { get; set; }
